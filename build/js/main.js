1dd97a40c2c5694969f9219c47feeea149def6b4
mozhnyy <48670931+mozhnyy@users.noreply.github.com>
2021-02-10 22:57:24 +0300

    Сделал адаптив шапки, добавил мобайл слайдер

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,31 @@
-"use strict";
+"use strict"; // Mobile menu
 
-$(function () {
-  // HEADER SLIDER
-  // 1. Проверяем существует ли элемент на странице?
-  if ($(".header-slider").length) {
-    //2. Если условие верно, начинаем писать нужный(необходимый)  код
-    // 2.1 Создаем/Объявляем переменную и сохраняем в нее наш слайдер(элемент)
-    var $headerSlider = $(".header-slider"); // 2.2 Создаем/Объявляем переменную и сохраняем в нее объект с параметрами для слайдера
+var menu = function () {
+  var hamburger = document.querySelector(".hamburger"),
+      menu = document.querySelector(".header__wrapper");
+  hamburger.addEventListener("click", function () {
+    this.classList.toggle("is-active");
+    menu.classList.toggle("header__wrapper-open");
+  });
+}(); // call menu
 
-    var options = {
-      arrows: false,
-      dots: true,
-      infinite: false,
-      appendDots: $headerSlider.next(),
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      variableWidth: true
-    };
-    $headerSlider.slick(options);
-  }
-});
+
+menu; // Проверка нахождения слайдера на нужной странице
+
+if (document.body.classList.contains("index-body")) {
+  // Слайдер
+  var headerSlider = function () {
+    var slider = document.querySelector(".slider"),
+        sliderDots = document.querySelectorAll("slider-controls__btn");
+    sliderDots.forEach(function (dot, dotIndex) {
+      dot.addEventListener("click", function () {
+        document.querySelector("slider-controls .slider-controls__btn-current").classList.remove("slider-controls__btn-current");
+        dot.classList.add("slider-controls__btn-current");
+        slider.style.transform = "translate(" + dotIndex * -33.33 + "%";
+      });
+    });
+  }(); // Call Slider
+
+
+  headerSlider;
+}
--- source/js/main.js
+++ source/js/main.js
@@ -1,22 +1,34 @@
-$(function () {
-	// HEADER SLIDER
-	// 1. Проверяем существует ли элемент на странице?
-	if ($(".header-slider").length) {
-		//2. Если условие верно, начинаем писать нужный(необходимый)  код
+"use strict";
+// Mobile menu
+var menu = (function () {
+	var hamburger = document.querySelector(".hamburger"),
+		menu = document.querySelector(".header__wrapper");
+	hamburger.addEventListener("click", function () {
+		this.classList.toggle("is-active");
+		menu.classList.toggle("header__wrapper-open");
+	});
+})();
+// call menu
+menu;
 
-		// 2.1 Создаем/Объявляем переменную и сохраняем в нее наш слайдер(элемент)
-		let $headerSlider = $(".header-slider");
-		// 2.2 Создаем/Объявляем переменную и сохраняем в нее объект с параметрами для слайдера
-
-		let options = {
-			arrows: false,
-			dots: true,
-			infinite: false,
-			appendDots: $headerSlider.next(),
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			variableWidth: true,
-		};
-		$headerSlider.slick(options);
-	}
-});
+// Проверка нахождения слайдера на нужной странице
+if (document.body.classList.contains("index-body")) {
+	// Слайдер
+	const headerSlider = (function () {
+		const slider = document.querySelector(".slider"),
+			sliderDots = document.querySelectorAll("slider-controls__btn");
+		sliderDots.forEach((dot, dotIndex) => {
+			dot.addEventListener("click", () => {
+				document
+					.querySelector(
+						"slider-controls .slider-controls__btn-current"
+					)
+					.classList.remove("slider-controls__btn-current");
+				dot.classList.add("slider-controls__btn-current");
+				slider.style.transform = "translate(" + dotIndex * -33.33 + "%";
+			});
+		});
+	})();
+	// Call Slider
+	headerSlider;
+}
